d0f172f2161774b32c36f21e324cba4824ad27c3
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-04-18 18:59:35 +0000

    fortune

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let who = ["Lovce is", "Your finances are", "Your health is", "Your Spirituality is"];
+  let action = ["looking up", "stagnate", "looking down"];
+  let what = ["but stay true to your path.", "but don't loose focus on your goals.", "and good fortune is around the corner, but be ready for an new adventure."];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  let randomIdx = Math.floor(Math.random() * who.length);
+  let whoR = who[randomIdx];
+  let actionR = action[Math.floor(Math.random() * action.length)];
+  let whatR = what[Math.floor(Math.random() * what.length)];
+  img.src = picsR;
+
+  document.getElementById("fortune").innerHTML =
+    whoR + " " + actionR + " " + whatR"
 };

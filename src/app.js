4f7931a364d8cb3f632c28f2bf2cbddd562b694c
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-04-19 17:11:26 +0000

    New Project

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-  let who = ["Lovce is", "Your finances are", "Your health is", "Your Spirituality is"];
-  let action = ["looking up", "stagnate", "looking down"];
-  let what = ["but stay true to your path.", "but don't loose focus on your goals.", "and good fortune is around the corner, but be ready for an new adventure."];
+let who = [
+  "Love is",
+  "Your finances are",
+  "Your health is",
+  "Your Spirituality is"
+];
+let action = ["looking up", "stagnate", "looking down"];
+let what = [
+  "but stay true to your path.",
+  "but don't loose focus on your goals.",
+  "and good fortune is around the corner.",
+  "but be ready for an new adventure."
+];
 
- 
-  let randomIdx = Math.floor(Math.random() * who.length);
-  let whoR = who[randomIdx];
-  let actionR = action[Math.floor(Math.random() * action.length)];
-  let whatR = what[Math.floor(Math.random() * what.length)];
-  img.src = picsR;
+let randomIdx = Math.floor(Math.random() * who.length);
+let whoR = who[randomIdx];
+let actionR = action[Math.floor(Math.random() * action.length)];
+let whatR = what[Math.floor(Math.random() * what.length)];
 
-  document.getElementById("fortune").innerHTML =
-    whoR + " " + actionR + " " + whatR"
-};
+document.getElementById("fortune").innerHTML =
+  whoR + " " + actionR + " " + whatR;
